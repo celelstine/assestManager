cd9a77a83ec77c71a6b2a8dd3c1f1279ea31431e
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-12 19:21:08 +0100

    import and intanciate uniqueid page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { UniqueIdPage } from '../unique-id/unique-id';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  uniqueIdPage = UniqueIdPage;
 
   constructor(public navCtrl: NavController) {
 
