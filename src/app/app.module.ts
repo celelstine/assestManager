c0068ed5d4b24bdc4ad399c06ee2360cddc5fd8e
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-15 11:32:23 +0100

    import and add dashboard page to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { CreateProfilePage } from '../pages/create-profile/create-profile';
 import { UniqueIdPage } from '../pages/unique-id/unique-id';
+import { DashboardPage } from '../pages/dashboard/dashboard';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     CreateProfilePage,
-    UniqueIdPage
+    UniqueIdPage,
+    DashboardPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     ListPage,
     CreateProfilePage,
-    UniqueIdPage
+    UniqueIdPage,
+    DashboardPage
   ],
   providers: [
     StatusBar,
