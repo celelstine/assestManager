bd4c9072c613c00a9205284345cc16c74cc208bb
celelstine <okorocelestine@gmail.com>
2018-05-15 17:24:35 +0100

    fix menu and the create profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,10 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
+  dashboardPage: any = DashboardPage;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Dashboard', component: DashboardPage }
-    ];
 
   }
 
@@ -40,6 +34,6 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(page);
   }
 }
