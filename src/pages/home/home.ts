e5731fb0d0ef8b088594d52740efc3ca84daf3f0
Nwoge_Eberechi <eberechi.daniel@yahoo.com>
2018-05-15 11:40:46 +0100

    add styles to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { UniqueIdPage } from '../unique-id/unique-id';
+import { DashboardPage } from '../dashboard/dashboard';
 
 @Component({
   selector: 'page-home',
@@ -10,9 +11,17 @@ import { UniqueIdPage } from '../unique-id/unique-id';
 export class HomePage {
 
   uniqueIdPage = UniqueIdPage;
+  dashboardPage = DashboardPage;
 
   constructor(public navCtrl: NavController) {
+  
 
+
+  }
+ 
+
+  open(){
+    this.navCtrl.push(DashboardPage);
   }
 
 }
